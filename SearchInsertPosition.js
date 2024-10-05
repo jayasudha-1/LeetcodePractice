ab7c53520ff491705542b88c357add4139c307d8
jayasudha-1 <138743005+jayasudha-1@users.noreply.github.com>
2024-10-04 21:01:03 -0700

    submission

---
+++ SearchInsertPosition.js
@@ -0,0 +1,19 @@
+var searchInsert = function(nums, target) {
+    let left=0;
+   let right=nums.length-1;
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        if(nums[mid]===target){
+            return mid;
+        }
+         else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+};
+let nums = [1,3,5,6], target = 5
+let result = searchInsert(nums,target);
+console.log(result);
