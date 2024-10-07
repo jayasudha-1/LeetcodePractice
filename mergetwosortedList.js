c9b5ff6513f430f11b9943110e674c262c8723a5
jayasudha-1 <138743005+jayasudha-1@users.noreply.github.com>
2024-10-06 22:42:29 -0700

    submitted recursive method for this code

--- mergetwosortedList.js
+++ mergetwosortedList.js
@@ -1,3 +1,4 @@
+//Iterative Method: time :O(m+n), space: O(1)
 var mergeTwoLists = function(list1, list2) {
     // Create a dummy node to help build the result list
     let dummy = new ListNode(-1);
@@ -20,4 +21,23 @@ var mergeTwoLists = function(list1, list2) {
     
     // Return the merged list, which starts from the next node of dummy
     return dummy.next;
+};
+
+/* Recursive method: time :O(m+n), space: O(m+n)
+var mergeTwoLists = function(list1, list2) {
+    // Base case: if either list is empty, return the other list
+    if (list1 === null) {
+        return list2;
+    } else if (list2 === null) {
+        return list1;
+    }
+    
+    // Recursively merge the lists based on the smaller node
+    if (list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
 };
