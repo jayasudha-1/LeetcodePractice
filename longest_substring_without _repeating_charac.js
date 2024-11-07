84dfb3301515c75a1ba5ceb8622f0da0b5e5374a
jayasudha-1 <138743005+jayasudha-1@users.noreply.github.com>
2024-11-06 20:49:03 -0800

    Updated longest_substring_without _repeating_charac.js

--- longest_substring_without _repeating_charac.js
+++ longest_substring_without _repeating_charac.js
@@ -10,6 +10,46 @@ var lengthOfLongestSubstring = function(s) {
     }
     return maxLength;
 };
+
+var lengthOfLongestSubstring = function(s) {
+    let maxLength=0;
+    let charMap=new Map();
+    for(let i=0,j=0;j<s.length;j++){
+        if(charMap.has(s[j])){
+            i=Math.max(charMap.get(s[j])+1,i);
+        }
+        maxLength=Math.max(maxLength, j - i + 1);
+        charMap.set(s[j], j);
+    }
+    return maxLength;
+};
+//test case
+s = "pwwkew"
+result=lengthOfLongestSubstring(s);
+console.log(result);
+
+
+/*--Another way of sliding window easier method
+
+var lengthOfLongestSubstring = function(s) {
+    let maxLength=0;
+    let left=0;
+    const set=new Set();
+
+    for(let right=0; right<s.length;right++){
+        while(set.has(s[right])){
+            set.delete(s[left]);
+            left++;
+        }
+        maxLength=Math.max(maxLength, right-left+1);
+        set.add(s[right]);
+    }
+    return maxLength;
+};
+*/
+
+
+
 //test case
 s = "pwwkew"
 result=lengthOfLongestSubstring(s);
