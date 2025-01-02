015ffa737f7824e1983dbee40767678e79b2bceb
Jayasudha <138743005+jayasudha-1@users.noreply.github.com>
2025-01-01 21:04:41 -0800

    Bit Manipulation submission for base7

---
+++ BitManipulation/Base7.js
@@ -0,0 +1,5 @@
+var convertToBase7 = function(num) {
+    return num.toString(7);
+   
+};
+//In JavaScript, we can use num.toString(base) to convert a number to different bases. The base can be any integer between 2 and 36.
