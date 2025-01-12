6ae6ca427d48c572a47d0279e6652f9ea0a9cf08
Jayasudha <138743005+jayasudha-1@users.noreply.github.com>
2025-01-11 23:49:23 -0800

    Updated coinChange.js

--- DynamicProgramming/coinChange.js
+++ DynamicProgramming/coinChange.js
@@ -1,7 +1,7 @@
 var coinChange = function(coins, amount) {
     // Initialize DP array with Infinity
     let dp = new Array(amount + 1).fill(Infinity);
-    dp[0] = 0; // Base case: 0 coins needed for amount 0
+    dp[0] = 0; // Base case: 0 coins needed for amount 0. first is 0, remaining filled with infinity. then that will be replaced later.
     
     // Iterate through each coin
     for (let coin of coins) {
@@ -9,7 +9,6 @@ var coinChange = function(coins, amount) {
             dp[i] = Math.min(dp[i], dp[i - coin] + 1);
         }
     }
-    
     // Check if amount is achievable
     return dp[amount] === Infinity ? -1 : dp[amount];
 };
